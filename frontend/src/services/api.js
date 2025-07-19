bba8bdb76373ed745c6de2219cc5373c3c604857
arikacips <1294330981oy@gmail.com>
2025-07-19 16:36:04 +0800

    Use relative baseURL for API requests in production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:5235/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
